1ed6b2ca20f818bb65b74c88630960fc9ee4bcab
Florence Njumbi <wambuinjumbi@gmail.com>
2022-10-27 09:54:21 +0300

    feat: add logic to save parking slots number and charges

--- src/main/kotlin/features/slot/data/repository/ParkingSlotsRepository.kt
+++ src/main/kotlin/features/slot/data/repository/ParkingSlotsRepository.kt
@@ -4,29 +4,49 @@ import core.database.AppKeys
 import core.database.Database
 import features.auth.data.UserModel
 import features.slot.data.models.ParkingSlotModel
+import java.util.*
 
 class ParkingSlotsRepository(private val database: Database) {
 
-    fun saveParkingSlots(userId: String, parkingSlotsNo: Int, amountChargedPerHour: Double): Pair<Boolean, String?> {
+    fun saveParkingSlots(
+        userId: String, parkingSlotsNo: Int, amountChargedPerHour: Double
+    ): Pair<Boolean, String?> {
         return try {
             // check if user exists
-            val users = database.getListOfUsingKey<UserModel>(AppKeys.userKey) ?: throw Exception("Users not found")
-            val user = users.find { it.id == userId } ?: throw Exception("User not found")
+            val checkUsers =
+                database.getListOfUsingKey<UserModel>(AppKeys.userKey) ?: throw Exception("Users do not exist")
+            checkUsers.find { it.id == userId } ?: throw Exception("User by that id does not exist")
 
-            // save to db
-            val parkingSlot = ParkingSlotModel(
-                userId = user.id,
+            // soot
+            val parkingSlotModel = ParkingSlotModel(
+                UUID.randomUUID().toString(),
+                userId = userId,
                 noOfParkingSlots = parkingSlotsNo,
                 amountChargedPerHour = amountChargedPerHour
             )
-            database.insertData(AppKeys.parkingSlot, listOf(parkingSlot))
-            Pair(true, "Parking slot and amount charged per hour added")
+
+            // update parking slots
+            var parkingSlots = database.getListOfUsingKey<ParkingSlotModel>(AppKeys.parkingSlot)
+            if (parkingSlots == null) {
+                parkingSlots = mutableListOf(parkingSlotModel)
+            } else {
+                // check if user has slots
+                val userSlots = parkingSlots.find { it.userId == userId }
+                if (userSlots != null) {
+                    // update slots
+                    userSlots.noOfParkingSlots = parkingSlotsNo
+                    userSlots.amountChargedPerHour = amountChargedPerHour
+                } else {
+                    // add as new
+                    parkingSlots
+                        .add(parkingSlotModel)
+                }
+            }
+            database.insertData(AppKeys.parkingSlot, parkingSlots)
+            Pair(true, "")
         } catch (e: Exception) {
             Pair(false, e.message)
         }
     }
 
-//    fun checkIfParkingSlotIsAlreadySet(userId: String): Boolean {
-//        val slots = database.getListOfUsingKey<ParkingSlotModel>(AppKeys.parkingSlot)
-//    }
 }
