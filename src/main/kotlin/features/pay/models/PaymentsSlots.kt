f1e095a94d7619431d6217675e52b6c52c849c83
Florence Njumbi <wambuinjumbi@gmail.com>
2022-10-27 09:56:40 +0300

    refactor: change properties for dataclass

--- src/main/kotlin/features/pay/models/PaymentsSlots.kt
+++ src/main/kotlin/features/pay/models/PaymentsSlots.kt
@@ -1,10 +1,11 @@
 package features.pay.models
 
-data class PaymentsSlots(
-    val id: Int,
-    val userId: Int,
-    val amount: Int?,
-    val startingTime: Int?,
-    val endingTime: Int?,
+data class PaymentModel(
+    val id: String,
+    val userId: Int? = null,
+    val carPlate: String,
+    var amount: Long? = null,
+    val startingTime: Long?,
+    var endingTime: Long? = null,
     val parkingSlot: String?
 )
