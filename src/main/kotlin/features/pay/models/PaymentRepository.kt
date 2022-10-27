8d3915e8fb583ae3c22711c17bd0dd580b4ec1b9
Florence Njumbi <wambuinjumbi@gmail.com>
2022-10-27 09:55:35 +0300

    feat: add logic to allow a user to pay and park their car

---
+++ src/main/kotlin/features/pay/models/PaymentRepository.kt
@@ -0,0 +1,95 @@
+package features.pay.models
+
+import core.database.AppKeys
+import core.database.Database
+import features.slot.data.models.ParkingSlotModel
+import java.util.*
+
+class PaymentRepository(val database: Database) {
+    // 1. check if parking business exists
+    // 2. check if parking slots is available
+    // 3. check if there is any pending payment
+    // 4. park car to a certain slot
+    fun park(
+        carNo: String,
+        parkingAreaNo: String,
+    ): Pair<Boolean, String?> {
+        return try {
+            // checking if parking business exists
+            val businesses = database.getListOfUsingKey<ParkingSlotModel>(AppKeys.parkingSlot)
+                ?: throw Exception("Business not found")
+            val parkingSlot =
+                businesses.find { it.id == parkingAreaNo } ?: throw Exception("Parking area by that id does not exist")
+
+            val paymentModel = PaymentModel(
+                id = UUID.randomUUID().toString(),
+                carPlate = carNo,
+                startingTime = Date().time,
+                parkingSlot = parkingSlot.id
+            )
+
+            // check for parking slot
+            var allPayments = database.getListOfUsingKey<PaymentModel>(AppKeys.payments)
+            if (allPayments != null) {
+                val occupiedSlots = allPayments.filter {
+                    it.parkingSlot == parkingSlot.id && it.startingTime != null && it.endingTime == null && it.amount == null
+                }.size
+                val remainingSlots = parkingSlot.noOfParkingSlots?.minus(occupiedSlots) ?: 0
+                if (remainingSlots > 0) {
+                    // available parking slot - park
+                    allPayments.add(paymentModel)
+                } else throw Exception("No available parking space")
+            } else {
+                allPayments = mutableListOf(paymentModel)
+            }
+
+            database.insertData(AppKeys.payments, allPayments)
+            Pair(true, "")
+        } catch (e: Exception) {
+            Pair(false, e.message)
+        }
+    }
+
+    //check existing record of number plate
+    // if car number is present query details using the car number plate
+    fun prepPay(
+        carNo: String
+    ): Triple<Boolean, String?, PaymentModel?> {
+        return try {
+            val carPayments = database.getListOfUsingKey<PaymentModel>(AppKeys.payments)
+                ?: throw Exception("No car parking record for $carNo")
+            // Checking if the car is parked in the parking area.
+            val carWantingToPay = carPayments.find { it.carPlate == carNo && it.endingTime == null && it.amount == null }
+                ?: throw Exception("Looks like your car has not been parked in this premises")
+
+            // update payment
+            val parkingSlot = database.getListOfUsingKey<ParkingSlotModel>(AppKeys.parkingSlot)
+            val carParkingSlot = parkingSlot?.find { it.id == carWantingToPay.parkingSlot }
+                ?: throw Exception("Unable to find parking area number")
+
+            carWantingToPay.endingTime = Date().time
+            carWantingToPay.amount = (((carWantingToPay.endingTime!! - carWantingToPay.startingTime!!) / (1000 * 60 * 60)) % 24);
+
+            Triple(true, "", carWantingToPay)
+        } catch (e: Exception) {
+            Triple(false, e.message, null)
+        }
+
+    }
+
+    fun pay(paymentModel: PaymentModel): Pair<Boolean, String?> {
+        return try {
+            var allPayments = database.getListOfUsingKey<PaymentModel>(AppKeys.payments)
+            if (allPayments != null) {
+                val carWantingToPay = allPayments.find { it.id == paymentModel.id } ?: throw Exception("Payment not found")
+                carWantingToPay.endingTime = paymentModel.endingTime
+                carWantingToPay.amount = paymentModel.amount
+
+                database.insertData(AppKeys.payments, allPayments)
+            }
+            Pair(true, "Thank you for parking with us")
+        } catch (e: Exception) {
+            Pair(false, e.message)
+        }
+    }
+}
