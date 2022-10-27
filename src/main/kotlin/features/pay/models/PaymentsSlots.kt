028626ce21db713f2626c1cf7684770d784f7b0d
Florence Njumbi <wambuinjumbi@gmail.com>
2022-10-27 13:09:04 +0300

    generated tests

--- src/main/kotlin/features/pay/models/PaymentsSlots.kt
+++ src/main/kotlin/features/pay/models/PaymentsSlots.kt
@@ -4,7 +4,7 @@ data class PaymentModel(
     val id: String,
     val userId: Int? = null,
     val carPlate: String,
-    var amount: Long? = null,
+    var amount: Double? = null,
     val startingTime: Long?,
     var endingTime: Long? = null,
     val parkingSlot: String?
